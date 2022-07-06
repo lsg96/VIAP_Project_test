152215a7bb3ed64c45b924963e557950d43d7abe
J1 <imjeabin@gmail.com>
2022-07-06 16:02:03 +0900

    데이터 베이스 모델 추가 밑 맵 추가. 차량정보 수집코드 추가

--- check/models.py
+++ check/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class CenterInfo(models.Model):
+    id = models.AutoField(primary_key= True)
+    name = models.CharField(max_length=50)
+    addr = models.CharField(max_length=150)
+    phone = models.CharField(max_length=50)
+    x = models.FloatField(default=0.0)
+    y = models.FloatField(default=0.0)
